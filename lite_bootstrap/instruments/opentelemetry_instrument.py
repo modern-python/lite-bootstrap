ce2849ac38786c6d6f6ed1c7af27e4e4645666de
Artur Shiriev <me@shiriev.ru>
2025-09-26 18:21:59 +0300

    use HOSTNAME for default otl container name

--- lite_bootstrap/instruments/opentelemetry_instrument.py
+++ lite_bootstrap/instruments/opentelemetry_instrument.py
@@ -26,7 +26,9 @@ class InstrumentorWithParams:
 @dataclasses.dataclass(kw_only=True, frozen=True)
 class OpentelemetryConfig(BaseConfig):
     opentelemetry_service_name: str | None = None
-    opentelemetry_container_name: str | None = dataclasses.field(default_factory=lambda: os.environ.get("HOSTNAME") or None)
+    opentelemetry_container_name: str | None = dataclasses.field(
+        default_factory=lambda: os.environ.get("HOSTNAME") or None
+    )
     opentelemetry_endpoint: str | None = None
     opentelemetry_namespace: str | None = None
     opentelemetry_insecure: bool = True
