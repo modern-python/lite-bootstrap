36bcf6364e301dea7249bf79bd24f145a540bebc
Artur Shiriev <lesnik512@gmail.com>
2026-04-08 13:18:39 +0300

    fix litestar imports

--- lite_bootstrap/bootstrappers/litestar_bootstrapper.py
+++ lite_bootstrap/bootstrappers/litestar_bootstrapper.py
@@ -98,7 +98,7 @@ class LitestarConfig(
     SentryConfig,
     SwaggerConfig,
 ):
-    application_config: "AppConfig" = dataclasses.field(default_factory=AppConfig)
+    application_config: "AppConfig" = dataclasses.field(default_factory=lambda: AppConfig())  # noqa: PLW0108
     opentelemetry_excluded_urls: list[str] = dataclasses.field(default_factory=list)
     prometheus_additional_params: dict[str, typing.Any] = dataclasses.field(default_factory=dict)
     swagger_extra_params: dict[str, typing.Any] = dataclasses.field(default_factory=dict)
